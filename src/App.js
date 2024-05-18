163303f9523e024fbf4c2d7afe082b482c0f876e
student <krysz@agh.edu.pl>
2024-05-18 11:11:38 +0200

    Second commit in project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'krysz@agh.edu.pl';
-
+    const [email, setEmail] = useState('krysz@agh.edu.pl');
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
     }
+
+    let message = <div>Ale masz krótki adres!</div>
+    if (email.length > 15) {
+        message = <div>Twój adres jest stanowczo za długi!</div>;
+    } else if (email.length > 5) {
+            message = <div>Twój adres jest git.</div>;
+    }
+
   return (
       <div>
 
         <h1>System do zapisów na zajęcia</h1>
           <h2>Twój e-mail to {email}</h2>
-          <input type="text" onChange={handleChange}/>
+          {message}
+          <input value={email} type="text" onChange={handleChange}/>
       </div>
 
   );
 
 }
+
+// "Ale masz krótki adres!"
+// "Twój adres jest git"
+// "Twój adres jest stanowczo za długi"
 export default App;
